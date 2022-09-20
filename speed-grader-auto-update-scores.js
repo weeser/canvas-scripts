ee8b67ab34dd327cf26e2a026904c63b61e016f9
Josh Weese <weeser@ksu.edu>
2022-09-20 11:55:21 -0500

    update

--- speed-grader-auto-update-scores.js
+++ speed-grader-auto-update-scores.js
@@ -1,5 +1,28 @@
-let update_scores = document.getElementsByClassName('btn btn-primary update-scores');
-document.querySelector(".question_input").addEventListener('change', (event) => {
-    update_scores.click();
+
+function update_scores (e){
+    document.getElementsByClassName('btn btn-primary update-scores').click();
     console.log("Scores updated");
-});
+}
+
+// Select the node that will be observed for mutations
+const target = document.getElementsByTagName('body');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+    let questions = document.querySelectorAll(".question_input");
+    for (var i = 0; i < questions.length; i++){
+        if (element.getAttribute('listener') !== 'true') {
+            document.querySelector(".question_input").addEventListener('change', update_scores);
+        }
+    }
+    
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(target, config);
